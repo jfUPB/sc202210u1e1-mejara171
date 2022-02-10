d0201f7494706745a0d2bc431af8178114598068
“mejara171” <martin.zuluaga@upb.edu.co>
2022-02-10 10:16:55 -0500

    poner informacion personal en el script

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Martin Jeronimo Zuluaga Orrego
+// 000255810
+// martin.zuluaga@upb.edu.co
 (START)
 @24576
 D=M
