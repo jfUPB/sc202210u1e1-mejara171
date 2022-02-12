3f6fe06843796d68d186651738065b02dbc1c549
“mejara171” <martin.zuluaga@upb.edu.co>
2022-02-12 13:20:45 -0500

    Agregando registros de pantalla y teclado

--- problem/program.asm
+++ problem/program.asm
@@ -1,35 +1,16 @@
 // Martin Jeronimo Zuluaga Orrego
 // 000255810
 // martin.zuluaga@upb.edu.co
-(START)
-@24576
-D=M
-@10
-D;JGT
-@16384
-D=M
-@16
-D;JLT
-@0
-0;JMP
-@16384
-D=M
-@16
-D;JEQ
-@0
-0;JMP
-@16384
-D=A
-@16
-M=D
-@24575
-D=A-D
-@0
-D;JLT
-@16
-A=M
-M=!M
-@16
-MD=M+1
-@20
-0;JMP
+(PANTALLA)
+    @16384
+    D=A
+    @0
+    M=D
+    @24576
+    D=M
+    @70
+    D=A-D
+    @16
+    D;JEQ
+    
+    
